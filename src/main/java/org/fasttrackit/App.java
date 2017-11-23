c770312439da9664849dd3abbeb91245de049508
MarcCristina <crys_20062006@yahoo.com>
2017-11-23 20:39:27 +0200

    simplified App.main

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -7,40 +7,11 @@ public class App {
 
     public static void main(String[] args) {
         Race race = new Race();
-
         Track track = new Track();
         track.setLenght(100);
-
         race.setTrack (track);
 
-
-        Engine engine = new Engine();
-        engine.setManufacturer ("Volvo");
-
-        Car car1 = new Car(engine);
-        car1.setName("Volvo");
-        car1.setColor("black");
-        car1.setFuelLevel(100.0);
-        car1.setMileage(13.5);
-        car1.setDoorCount (4);
-
-        Engine engine2 = new Engine();
-        engine2.setManufacturer ("Mercedes");
-
-        System.out.println("car is:" + car1.getName());
-
-        Car car2 = new Car(engine2);
-        car2.setName("Mercedes");
-        car2.setColor("blue");
-        car2.setFuelLevel(115.00);
-        car2.setMileage(11.5);
-        car2.setDoorCount (4);
-
-
-        Vehicle vehicle = new Vehicle();
-        vehicle.accelerate(20.5);
-
-        AutoVehicle autoVehicle = new AutoVehicle(new Engine());
+        race.start();
 
 
 
--- src/main/java/org/fasttrackit/Race.java
+++ src/main/java/org/fasttrackit/Race.java
@@ -6,6 +6,35 @@ public class Race {
     private Car firstCompetitor;
     private Car secondCompetitor;
 
+    public void start (){
+        Engine engine = new Engine();
+        engine.setManufacturer ("Volvo");
+
+        firstCompetitor = createCompetitor( engine,"Mercedes", "red", 40, 8.5, 4 );
+
+        Engine secondEngine = new Engine();
+        secondEngine.setManufacturer ("Toyota");
+
+        secondCompetitor = createCompetitor(engine, "Honda", "black", 40, 10.5, 2 );
+
+
+    }
+
+    // parameters contain car prefix just to demo they can have any name
+    private Car createCompetitor(Engine carEngine, String carName, String carColor, double carFuelLevel, double carMileage, int carDoorCount) {
+        Car competitor = new Car(carEngine);
+        competitor.setName(carName);
+        competitor.setColor(carColor);
+        competitor.setFuelLevel(carFuelLevel);
+        competitor.setMileage(carMileage);
+        competitor.setDoorCount(carDoorCount);
+
+        return competitor;
+
+
+    }
+
+
     public Track getTrack() {
         return track;
     }
